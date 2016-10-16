9cd48b96a3a857110dc72f2609777ff1bb51cc7e
Dominik Niedermaier <galaxycrysis11@gmail.com>
2016-10-16 02:37:26 +0200

    addes menubar and import functions

--- MainGui.py
+++ MainGui.py
@@ -5,4 +5,25 @@ class mainWindow(QtGui.QMainWindow):
     def __init__(self):
         super(mainWindow,self).__init__()
         uic.loadUi("main.ui",self)
+        self.setWindowTitle("Data Science Platform")
+        #init variables
+        self.dataList = list()
+        self.nameList = list()
+        self.addMenu()
+
+        self.show()
+
+    #init the Menu bar for the Gui
+    def addMenu(self):
+        importMenu = self.menuBar().addMenu("Import Dataset")
+        importMenu.addAction("Import from Database", self.importDB)
+        importMenu.addAction("Import from local File",self.importFile)
+
+    def importDB(self):
+        print("")
+
+    def importFile(self):
+        print("")
+
+
 
